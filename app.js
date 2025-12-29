24ad02e8044a83c7502fed92d56045c66e6cdf53
aarontark <tarkington.aaron@gmail.com>
2025-12-29 11:45:52 -0600

    refactored search results function by adding helper functions

--- app.js
+++ app.js
@@ -9,45 +9,55 @@ async function searchResults(searchStr) {
         return;
     }
 
-    // loading bar DOM code
-    let loadingContainer = document.createElement("div");
-    const loadingImg = document.createElement("img");
-    const loadingText = document.createElement("p");
-    loadingImg.src = "assets/images/icon-loading.svg";
-    loadingImg.classList.add('loading-img');
-    loadingText.innerHTML = "Search in progress";
-    loadingContainer.classList.add('result-container');
-    loadingContainer.appendChild(loadingImg);
-    loadingContainer.appendChild(loadingText);
-    resultsBar.appendChild(loadingContainer);
-    resultsBar.style.display = 'flex';
+    searchLoadRender(resultsBar);
 
     const geocodingDataRaw = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${searchStr}&count=10&language=en&format=json`);
     const geoCodingDataClean = await geocodingDataRaw.json();
     const geoCodingData = geoCodingDataClean.results;
+    
     loadingContainer = document.querySelector('.result-container');
     resultsBar.removeChild(loadingContainer);
     resultsBar.style.display = 'none';
-    for (i = 0; i < 5; i++) {
-        const locationContainer = document.createElement('div');
-        const countryFlag = document.createElement("img")
-        countryFlag.classList.add('country-flag');
-        countryFlag.src = `https://flagcdn.com/256x192/${geoCodingData[i].country_code.toLowerCase()}.png`;
-        const locationData = document.createElement('p');
-        locationData.innerHTML = `${geoCodingData[i].name}, ${geoCodingData[i].country}`;
-        locationContainer.appendChild(countryFlag);
-        locationContainer.appendChild(locationData);
-        locationContainer.classList.add('result-container');
-        locationContainer.dataset.latitude = geoCodingData[i].latitude;
-        locationContainer.dataset.longitude = geoCodingData[i].longitude;
-        locationContainer.addEventListener('mousedown', () => {
-            searchBtn.dataset.latitude = locationContainer.dataset.latitude;
-            searchBtn.dataset.longitude = locationContainer.dataset.longitude;
-            searchBar.value = locationData.innerHTML;
-        })
-        resultsBar.appendChild(locationContainer);
-    }
+    searchResultRender(resultsBar, geoCodingData);
     resultsBar.style.display = 'flex';
+}
+
+const searchLoadRender = (resultsBar) => {
+    const loadingContainer = document.createElement("div");
+    const loadingImg = document.createElement("img");
+    const loadingText = document.createElement("p");
+    loadingImg.src = "assets/images/icon-loading.svg";
+    loadingImg.classList.add("loading-img");
+    loadingText.innerHTML = "Search in progress";
+    loadingContainer.classList.add("result-container");
+    loadingContainer.appendChild(loadingImg);
+    loadingContainer.appendChild(loadingText);
+    resultsBar.appendChild(loadingContainer);
+    resultsBar.style.display = "flex";
+}
+
+const searchResultRender = (resultsBar, geoCodingData) => {
+    for (i = 0; i < 5; i++) {
+      const locationContainer = document.createElement("div");
+      const countryFlag = document.createElement("img");
+      countryFlag.classList.add("country-flag");
+      countryFlag.src = `https://flagcdn.com/256x192/${geoCodingData[
+        i
+      ].country_code.toLowerCase()}.png`;
+      const locationData = document.createElement("p");
+      locationData.innerHTML = `${geoCodingData[i].name}, ${geoCodingData[i].country}`;
+      locationContainer.appendChild(countryFlag);
+      locationContainer.appendChild(locationData);
+      locationContainer.classList.add("result-container");
+      locationContainer.dataset.latitude = geoCodingData[i].latitude;
+      locationContainer.dataset.longitude = geoCodingData[i].longitude;
+      locationContainer.addEventListener("mousedown", () => {
+        searchBtn.dataset.latitude = locationContainer.dataset.latitude;
+        searchBtn.dataset.longitude = locationContainer.dataset.longitude;
+        searchBar.value = locationData.innerHTML;
+      });
+      resultsBar.appendChild(locationContainer);
+    }
 }
 
 searchBar.addEventListener('input', () => {
